f05d5aadd9b4f606e1a90f6e6065ad71ec7e27bd
AlexYalovyy <YalovyyAlex@gmail.com>
2018-11-04 15:50:21 +0200

    Fix index i in insertion sort

--- Algorithms/insertionSort/index.js
+++ Algorithms/insertionSort/index.js
@@ -1,12 +1,12 @@
 function insertionSort(array) {
   var insertedItem;
   var j;
-  for (let i = 1; i < array.length; i++) {
-    insertedItem = array[i];
+  for (let i = 0; i < array.length; i++) {
+    insertedItem = array[i];  // Every time select another element
     j = i - 1;
 
     while(j >= 0 && array[j] > insertedItem) {
-      array[j+1] = array[j];
+      array[j+1] = array[j];  // Move sorted arr right to free space
       j--;
     }
     array[j + 1] = insertedItem;
